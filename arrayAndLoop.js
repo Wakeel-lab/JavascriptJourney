4ff584db68327596f6323813a93f69ee3a19ee63
Wakeel Saliu <wakeelsaliu1@gmail.com>
2021-08-16 16:57:53 +0100

    EvenNumbersArray

---
+++ arrayAndLoop.js
@@ -0,0 +1,11 @@
+let myEvenNumbersArray = [];
+
+for(let i = 0; i <= 5; i++){
+    myEvenNumbersArray[i] = i * 2;
+}
+
+console.log(myEvenNumbersArray);
+
+for(let i = 0; i < myEvenNumbersArray.length; i++){
+    console.log(myEvenNumbersArray[i]);
+}
